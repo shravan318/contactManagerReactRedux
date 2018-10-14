38036b58825106ef8e7330d2f657a039613af88e
shravan318 <shravan.318@gmail.com>
2018-10-14 21:34:30 +0530

    Add- add contact functionality

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -39,7 +39,7 @@ export default function(state = initialState, action) {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [action.payload, state.contacts]
+        contacts: [action.payload, ...state.contacts]
       };
     default:
       return state;
