13aed9c449201d5ffee7c41a7acfa701bdc9110a
shravan318 <shravan.318@gmail.com>
2018-10-14 22:59:47 +0530

    Add json place holder fetch and delete request

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,15 +1,21 @@
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "./types";
+import axios from "axios";
 
-export const getContacts = () => {
-  return {
-    type: GET_CONTACTS
-  };
+export const getContacts = () => async dispatch => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  dispatch({
+    type: GET_CONTACTS,
+    payload: response.data
+  });
 };
-export const deleteContact = id => {
-  return {
+export const deleteContact = id => async dispatch => {
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  dispatch({
     type: DELETE_CONTACT,
     payload: id
-  };
+  });
 };
 export const addContact = contact => {
   return {
